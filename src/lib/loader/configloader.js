824b1ce43289ba0bcb10e8885d6273597217a154
Andreas Hölzl <andy.woodly@googlemail.com>
2012-09-03 12:30:45 +0200

    removed deftjs dep

--- src/lib/loader/configloader.js
+++
@@ -1,9 +0,0 @@
-var config = (function() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'config.json', false);
-    xhr.send(null);
-
-    var options = eval("(" + xhr.responseText + ")"),
-        config = options.config || {};
-    return config;
-})();
