c559bf5030a133f3ecd949133feca6f4f968f697
Oleg Buss <oleg.buss@canoo.com>
2012-09-05 08:52:15 +0200

    added navigation bar controller test

--- test/app/controller/TodoNavigationBarDeftController-test.js
+++ test/app/controller/TodoNavigationBarDeftController-test.js
@@ -4,12 +4,37 @@ var refute = buster.assertions.refute;
 
 buster.testCase("TodoNavigationDeftController",  {
     setUp: function() {
+        var me = this;
+        this.fireEventSpy = me.spy();
+        this.addEventListenerSpy = me.spy();
+
+        Deft.Injector.configure({
+            eventBus: {
+                fn: function() {
+                    return {
+                        fireEvent: me.fireEventSpy,
+                        addEventListener: me.addEventListenerSpy
+                    }
+                }
+            }
+        });
+
         this.navigation = Ext.create('Todo.view.TodoNavigation');
         Ext.Viewport.add(this.navigation);
+    },
+
+    tearDown: function() {
+        Ext.Viewport.remove(this.navigation);
     },
 
     "todo navigation bar controller is set" : function() {
         assert(this.navigation.getController());
         assert.equals(Ext.getClassName(this.navigation.getController()), 'Todo.controller.TodoNavigationBarController');
+    },
+
+    "todo navigation bar controller registers eventlisteners" : function() {
+        var controller = this.navigation.getController();
+        controller.onSaveTodoListButtonTap();
+        assert.calledOnceWith(this.fireEventSpy, Todo.Event.SAVE_TODO_LIST);
     }
 });
